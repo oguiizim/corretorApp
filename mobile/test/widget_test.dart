d10f65c631514028fc662c6ee288fd4de4ecb041
Guilherme-Almeida <guilhermecordeiroyt@gmail.com>
2026-05-16 18:37:29 -0300

    initial commit

---
+++ mobile/test/widget_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:mobile/main.dart';
+
+void main() {
+  testWidgets('renderiza tela de autenticacao', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
+
+    await tester.pumpWidget(const CorretorApp());
+    await tester.pumpAndSettle();
+
+    expect(find.text('Corretor de Imoveis'), findsOneWidget);
+    expect(find.text('Entrar'), findsWidgets);
+  });
+}
